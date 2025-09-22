4aaa153a7704c34bea8d11bf99ded753c161e794
Don Olmstead <me@donolmstead.dev>
2025-09-22 14:47:37 -0700

    models: generate enums
    Generate all the `enum` types within the library.

---
+++ lib/src/models/layout_grid_pattern.dart
@@ -0,0 +1,17 @@
+// Generated from v0.33.0 of the Figma REST API specification
+
+import 'package:json_annotation/json_annotation.dart';
+
+/// Orientation of the grid as a string enum
+///
+/// - `COLUMNS`: Vertical grid
+/// - `ROWS`: Horizontal grid
+/// - `GRID`: Square grid
+enum LayoutGridPattern {
+  @JsonValue('COLUMNS')
+  columns,
+  @JsonValue('ROWS')
+  rows,
+  @JsonValue('GRID')
+  grid,
+}
--- lib/src/models/scale_mode.dart
+++ lib/src/models/scale_mode.dart
@@ -1,20 +1,15 @@
+// Generated from v0.33.0 of the Figma REST API specification
+
 import 'package:json_annotation/json_annotation.dart';
 
 /// Image scaling mode.
 enum ScaleMode {
-  /// Scale the image to fill the frame.
   @JsonValue('FILL')
   fill,
-
-  /// Scale the image to fit the frame while preserving the original aspect ratio.
   @JsonValue('FIT')
   fit,
-
-  /// Tile the image repeatedly at its original size, without scaling.
   @JsonValue('TILE')
   tile,
-
-  /// Stretch the image to fit the frame.
   @JsonValue('STRETCH')
   stretch,
 }
